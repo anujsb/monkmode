4ca8c48f673d4ec3550d80b828b55411cc27b27b
anuj <anujsbhuyar@gmail.com>
2024-07-30 10:13:15 +0530

    init clerk setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { Hero } from "@/components";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center bg-black-100">
-      <Hero />
+    <main>
+      hi
     </main>
   );
 }
